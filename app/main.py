d70a49a6b0f5b7a034a99c172432dd7a5f756e24
PaniShews <kpetruh@gmail.com>
2026-04-05 22:34:56 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -4,11 +4,12 @@ from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(movie: str,
-                 customers: list,
-                 hall_number: int,
-                 cleaner: str
-                 ) -> None:
+def cinema_visit(
+        movie: str,
+        customers: list,
+        hall_number: int,
+        cleaner: str
+) -> None:
     cust_list = []
 
     for cust in customers:
@@ -27,19 +28,3 @@ def cinema_visit(movie: str,
     hall.movie_session(movie_name=movie,
                        customers=cust_list,
                        cleaning_staff=cleaning_staff)
-
-    customers = [
-        {"name": "Bob", "food": "Coca-cola"},
-        {"name": "Alex", "food": "popcorn"}
-    ]
-    hall_number = 5
-    cleaner_name = "Anna"
-    movie = "Madagascar"
-    cinema_visit(customers=customers, hall_number=hall_number, cleaner=cleaner_name, movie=movie)
-    # Cinema bar sold Coca-cola to Bob.
-    # Cinema bar sold popcorn to Alex.
-    # "Madagascar" started in hall number 5.
-    # Bob is watching "Madagascar".
-    # Alex is watching "Madagascar".
-    # "Madagascar" ended.
-    # Cleaner Anna is cleaning hall number 5.
