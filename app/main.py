654d857eeb470dece1ac54cc1059682779144ca7
PaniShews <kpetruh@gmail.com>
2026-04-05 21:53:09 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -9,15 +9,37 @@ def cinema_visit(movie: str,
                  hall_number: int,
                  cleaner: str
                  ) -> None:
+    cust_list = []
+
     for cust in customers:
-        customer_instances = Customer(name=cust['name'], food=cust['food'])
+        customer_instances = Customer(
+            name=cust["name"],
+            food=cust["food"]
+        )
 
         CinemaBar.sell_product(
             product=customer_instances.food,
             customer=customer_instances
         )
+        cust_list.append(customer_instances)
+    hall = CinemaHall(hall_number)
+    cleaning_staff = Cleaner(cleaner)
+    hall.movie_session(movie_name=movie,
+                       customers=cust_list,
+                       cleaning_staff=cleaning_staff)
 
-    hall = CinemaHall(hall_number=hall_number)
-    cleaning_staff = Cleaner(name=cleaner)
-
-    hall.movie_session(movie_name=movie, customers=customer_instances, cleaning_staff=cleaning_staff)
+    customers = [
+        {"name": "Bob", "food": "Coca-cola"},
+        {"name": "Alex", "food": "popcorn"}
+    ]
+    hall_number = 5
+    cleaner_name = "Anna"
+    movie = "Madagascar"
+    cinema_visit(customers=customers, hall_number=hall_number, cleaner=cleaner_name, movie=movie)
+    # Cinema bar sold Coca-cola to Bob.
+    # Cinema bar sold popcorn to Alex.
+    # "Madagascar" started in hall number 5.
+    # Bob is watching "Madagascar".
+    # Alex is watching "Madagascar".
+    # "Madagascar" ended.
+    # Cleaner Anna is cleaning hall number 5.
